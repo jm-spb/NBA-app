003c0b9feb248b1dadbb7f24d54ad5a8fb94d08e
ivan <tuchel.ivan@gmail.com>
2021-12-16 18:17:25 +0300

    Header: fix layout

---
+++ src/components/Header/HeaderTeams/Division.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Menu } from 'antd';
+import { ITeamsRenderData } from '../../../types/teamsHeader';
+
+// import { nbaApi } from '../../services/NbaService';
+
+interface IDivisionProps {
+  divName: string;
+  divTeams: ITeamsRenderData[];
+}
+
+const Division = ({ divName, divTeams }: IDivisionProps): JSX.Element => {
+  // const { data: teams } = nbaApi.useFetchTeamsQuery('');
+
+  // const divTeams =
+  //   allTeams &&
+  //   allTeams.filter(
+  //     (el: any) => el.divName && el.divName.toLowerCase() === divName.toLowerCase()
+  //   );
+
+  console.log(divTeams);
+
+  return (
+    <Menu.ItemGroup className="division" title={divName}>
+      {divTeams?.map((team) => (
+        <Menu.Item className="team" key={team.teamId}>
+          <img
+            src={team.logo}
+            width={25}
+            height={25}
+            alt={`${team.fullName} Logo`}
+            // loading="lazy"
+            className="team-logo"
+          />
+          <span className="team-fullName">{team.fullName}</span>
+        </Menu.Item>
+      ))}
+    </Menu.ItemGroup>
+  );
+};
+
+export default React.memo(Division);
