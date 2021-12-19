c9a31e2c7f9402ea626a3409702f60de79de2864
ivan <tuchel.ivan@gmail.com>
2021-12-19 16:59:49 +0300

    MainCarousel: refactor carousel slides

--- src/components/MainCarousel/index.tsx
+++ src/components/MainCarousel/index.tsx
@@ -1,36 +1,51 @@
 import React from 'react';
 
 import './MainCarousel.scss';
-// import 'swiper/swiper.scss';
 import 'swiper/modules/pagination/pagination.scss';
 
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
+import { Button } from 'antd';
 
-import nba_75 from '../../assets/nba_75.webp';
-import nba_league_pass from '../../assets/nba_league_pass.webp';
-import nba_store from '../../assets/nba_store.webp';
-import nba_tickets from '../../assets/nba_tickets.webp';
-import nba_cares from '../../assets/nba_cares.webp';
+import { mainCarouselContent } from '../../scripts/mainCarouselContent';
 
 SwiperCore.use([Pagination, Autoplay]);
-
-// This info should be from backend, but API doesn't provide such info
-const CarouselTextContent = [
-  'This is the NBA 75th Anniversary Team',
-  'Explore the World of NBA with League Pass',
-  'Checkout the Official NBA Store',
-  'Official Source of Authentic NBA Tickets',
-  'NBA Cares - the League’s Global Social Responsibility Program',
-];
 
 const MainCarousel = (): JSX.Element => {
   const pagination = {
     clickable: true,
-    renderBullet: function (index: any, className: any) {
-      return `<span class=${className}><p>${CarouselTextContent[index]}</p></span>`;
+    renderBullet: function (index: number, className: string) {
+      return `<span class=${className}><p>${mainCarouselContent[index].paginationText}</p></span>`;
     },
   };
+
+  const renderMainCarouselSlides = mainCarouselContent.map(
+    ({ heading, paragraph_1, paragraph_2, link, image, paginationText }) => (
+      <SwiperSlide key={heading}>
+        <div className="main-slide">
+          <div className="main-slide-content">
+            <h1>{heading}</h1>
+            <p>{paragraph_1}</p>
+            <p>{paragraph_2}</p>
+            <Button
+              className="main-slide-button"
+              type="link"
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+              size="large"
+              shape="round"
+            >
+              Read More
+            </Button>
+          </div>
+          <div className="main-slide-image">
+            <img src={image} alt={paginationText} loading="lazy" />
+          </div>
+        </div>
+      </SwiperSlide>
+    )
+  );
 
   return (
     <div className="main">
@@ -40,91 +55,7 @@ const MainCarousel = (): JSX.Element => {
         // watchSlidesProgress={true}
         className="main-content"
       >
-        <SwiperSlide>
-          <div className="main-slide">
-            <div className="main-slide-content">
-              <h1>head head head head head head head head head</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                adipisci perferendis unde praesentium mollitia obcaecati, rerum vitae
-                nihil veniam, aperiam maxime iusto. Officiis nostrum voluptatum mollitia,
-                error id fuga minus.
-              </p>
-              <button>Button</button>
-            </div>
-            <div className="main-slide-image">
-              <img src={nba_75} alt="nba_75" loading="lazy" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main-slide">
-            <div className="main-slide-content">
-              <h1>head head head head head head head head head</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                adipisci perferendis unde praesentium mollitia obcaecati, rerum vitae
-                nihil veniam, aperiam maxime iusto. Officiis nostrum voluptatum mollitia,
-                error id fuga minus.
-              </p>
-              <button>Button</button>
-            </div>
-            <div className="main-slide-image">
-              <img src={nba_league_pass} alt="nba_league_pass" loading="lazy" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main-slide">
-            <div className="main-slide-content">
-              <h1>head head head head head head head head head</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                adipisci perferendis unde praesentium mollitia obcaecati, rerum vitae
-                nihil veniam, aperiam maxime iusto. Officiis nostrum voluptatum mollitia,
-                error id fuga minus.
-              </p>
-              <button>Button</button>
-            </div>
-            <div className="main-slide-image">
-              <img src={nba_store} alt="nba_store" loading="lazy" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main-slide">
-            <div className="main-slide-content">
-              <h1>head head head head head head head head head</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                adipisci perferendis unde praesentium mollitia obcaecati, rerum vitae
-                nihil veniam, aperiam maxime iusto. Officiis nostrum voluptatum mollitia,
-                error id fuga minus.
-              </p>
-              <button>Button</button>
-            </div>
-            <div className="main-slide-image">
-              <img src={nba_tickets} alt="nba_tickets" loading="lazy" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main-slide">
-            <div className="main-slide-content">
-              <h1>head head head head head head head head head</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                adipisci perferendis unde praesentium mollitia obcaecati, rerum vitae
-                nihil veniam, aperiam maxime iusto. Officiis nostrum voluptatum mollitia,
-                error id fuga minus.
-              </p>
-              <button>Button</button>
-            </div>
-            <div className="main-slide-image">
-              <img src={nba_cares} alt="nba_cares" loading="lazy" />
-            </div>
-          </div>
-        </SwiperSlide>
+        {renderMainCarouselSlides}
       </Swiper>
     </div>
   );
