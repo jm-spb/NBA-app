277372b7c688684148004a7d5bb3a186ee5f4dce
ivan <tuchel.ivan@gmail.com>
2021-12-19 16:08:46 +0300

    create content for Main Carousel

---
+++ src/scripts/mainCarouselContent.ts
@@ -0,0 +1,59 @@
+import nba_75 from '../assets/nba_75.webp';
+import nba_league_pass from '../assets/nba_league_pass.webp';
+import nba_store from '../assets/nba_store.webp';
+import nba_tickets from '../assets/nba_tickets.webp';
+import nba_cares from '../assets/nba_cares.webp';
+
+export const mainCarouselContent = [
+  {
+    heading: 'NBA 75th Anniversary Team announced',
+    paragraph_1:
+      'A total of 158 NBA championships and 730 NBA All-Star selections. A combined 110 Kia NBA Most Valuable Player Awards and Bill Russell NBA Finals MVP Awards. More than 1.5 million points scored. The NBA 75th Anniversary Team was selected by a blue-ribbon panel of current and former NBA players, coaches, general managers and team and league executives, WNBA legends and sportswriters and broadcasters. ',
+    paragraph_2:
+      'Voters were asked to select the 75 Greatest Players in NBA History without regard to position. Panelists did not rank their selections. Current and former players were not allowed to vote for themselves.',
+    link: 'https://www.nba.com/75',
+    image: { nba_75 },
+    paginationText: 'This is the NBA 75th Anniversary Team',
+  },
+  {
+    heading: 'Stream live and on-demand games on the NBA app or on nba.com.',
+    paragraph_1:
+      "NBA League Pass is a regular-season package you can subscribe to with Prime Video Channels. It provides subscribers access to games they would not be able to watch on cable or local over-the-air stations. NBA League Pass subscribers get live access to Home, Away, and Mobile Optimized broadcast feeds for all regular season games that aren't subject to a regional or national blackout. ",
+    paragraph_2:
+      'NBA League Pass also includes on-demand replays for each broadcast, and an 8-12 minute condensed replay of each game.',
+    link: 'https://watch.nba.com/streaming-subscriptions',
+    image: { nba_league_pass },
+    paginationText: 'Explore the World of NBA with League Pass',
+  },
+  {
+    heading: 'NBA Apparel and Jerseys at www.nbastore.eu',
+    paragraph_1:
+      'Choose from the assortment at the NBA Store to find the perfect NBA merchandise for your collection. In addition to NBA jerseys, t-shirts and sweatshirts, our shop offers tons of officially licensed NBA collectibles that you can show off in your home or office. ',
+    paragraph_2:
+      'Find the newest player merchandise in a wide range of sizes so you and your fellow fans can represent your favorite basketball team in authentic NBA style. Browse the NBA Store for the greatest assortment of basketball jerseys and clothing online.',
+    link: 'https://www.nbastore.eu/en/',
+    image: { nba_store },
+    paginationText: 'Checkout the Official NBA Store',
+  },
+  {
+    heading: 'Authentic and Verified. All Tickets. One Site.',
+    paragraph_1:
+      'The only trustworthy source for authentic NBA Tickets: 100% guaranteed to get fans into every game for every team. Search tickets available from the team or browse the resale marketplace. ',
+    paragraph_2:
+      'NBATickets.com is the NBA’s one-stop shop for all ticket types for all teams: Fans can link out to buy single game tickets directly from the team, browse resale options, or view more options on team ticket pages, including promotions and deals.',
+    link: 'https://nbatickets.nba.com/home/',
+    image: { nba_tickets },
+    paginationText: 'Official Source of Authentic NBA Tickets',
+  },
+  {
+    heading:
+      'NBA and society: education, youth and family development, health and wellness.',
+    paragraph_1:
+      'NBA Cares is the league’s global social responsibility program that builds on the NBA’s mission of addressing important social issues in the U.S. and around the world. ',
+    paragraph_2:
+      'NBA Cares programs and participants have provided more than 5.8 million hours of hands-on service, created more than 1,650 places where kids and families can live, learn or play and engaged more than 60 million youth in basketball programs in communities around the world. Internationally, NBA Cares has created more than 332 places where kids and families can live, learn, or play in 40 countries.',
+    link: 'https://cares.nba.com/',
+    image: { nba_cares },
+    paginationText: 'NBA Cares - the League’s Global Social Responsibility Program',
+  },
+];
