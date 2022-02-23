082f3353520c6a97bfcf30b3b2c0c422b0849f34
ivan <tuchel.ivan@gmail.com>
2022-02-23 14:41:56 +0300

    add nickName prop

--- src/utils/headerTeamsDropdown.ts
+++ src/utils/headerTeamsDropdown.ts
@@ -6,25 +6,30 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Boston Celtics',
+        nickName: 'celtics',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png',
       },
       {
         teamName: 'Brooklyn Nets',
+        nickName: 'nets',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Brooklyn_Nets_newlogo.svg/130px-Brooklyn_Nets_newlogo.svg.png',
       },
       {
         teamName: 'New York Knicks',
+        nickName: 'knicks',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/d/dc/NY_Knicks_Logo_2011.png',
       },
       {
         teamName: 'Philadelphia 76ers',
+        nickName: 'sixers',
         teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/4/48/76ers_2016.png',
       },
       {
         teamName: 'Toronto Raptors',
+        nickName: 'raptors',
         teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/8/89/Raptors2015.png',
       },
     ],
@@ -34,26 +39,31 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Chicago Bulls',
+        nickName: 'bulls',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d1/Bulls_de_Chicago_logo.svg/1200px-Bulls_de_Chicago_logo.svg.png',
       },
       {
         teamName: 'Cleveland Cavaliers',
+        nickName: 'cavaliers',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/0/06/Cavs_de_Cleveland_logo_2017.png/150px-Cavs_de_Cleveland_logo_2017.png',
       },
       {
         teamName: 'Detroit Pistons',
+        nickName: 'pistons',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Detroit_Pistons_primary_logo_2017.png/150px-Detroit_Pistons_primary_logo_2017.png',
       },
       {
         teamName: 'Indiana Pacers',
+        nickName: 'pacers',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/c/cf/Pacers_de_l%27Indiana_logo.svg/1180px-Pacers_de_l%27Indiana_logo.svg.png',
       },
       {
         teamName: 'Milwaukee Bucks',
+        nickName: 'bucks',
         teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/3/34/Bucks2015.png',
       },
     ],
@@ -63,25 +73,30 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Atlanta Hawks',
+        nickName: 'hawks',
         teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png',
       },
       {
         teamName: 'Charlotte Hornets',
+        nickName: 'hornets',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/f/f3/Hornets_de_Charlotte_logo.svg/1200px-Hornets_de_Charlotte_logo.svg.png',
       },
       {
         teamName: 'Miami Heat',
+        nickName: 'heat',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/1/1c/Miami_Heat_-_Logo.svg/1200px-Miami_Heat_-_Logo.svg.png',
       },
       {
         teamName: 'Orlando Magic',
+        nickName: 'magic',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/b/bd/Orlando_Magic_logo_2010.png',
       },
       {
         teamName: 'Washington Wizards',
+        nickName: 'wizards',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/archive/d/d6/20161212034849%21Wizards2015.png',
       },
@@ -92,26 +107,31 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Denver Nuggets',
+        nickName: 'nuggets',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/3/35/Nuggets_de_Denver_2018.png/180px-Nuggets_de_Denver_2018.png',
       },
       {
         teamName: 'Minnesota Timberwolves',
+        nickName: 'timberwolves',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d9/Timberwolves_du_Minnesota_logo_2017.png/200px-Timberwolves_du_Minnesota_logo_2017.png',
       },
       {
         teamName: 'Oklahoma City Thunder',
+        nickName: 'thunder',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/4/4f/Thunder_d%27Oklahoma_City_logo.svg/1200px-Thunder_d%27Oklahoma_City_logo.svg.png',
       },
       {
         teamName: 'Portland Trail Blazers',
+        nickName: 'blazers',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/en/thumb/2/21/Portland_Trail_Blazers_logo.svg/1200px-Portland_Trail_Blazers_logo.svg.png',
       },
       {
         teamName: 'Utah Jazz',
+        nickName: 'jazz',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/3/3b/Jazz_de_l%27Utah_logo.png',
       },
@@ -122,25 +142,34 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Golden State Warriors',
+        nickName: 'warriors',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Warriors_de_Golden_State_logo.svg/1200px-Warriors_de_Golden_State_logo.svg.png',
       },
       {
         teamName: 'Los Angeles Clippers',
+        nickName: 'clippers',
+
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/d/d6/Los_Angeles_Clippers_logo_2010.png',
       },
       {
         teamName: 'Los Angeles Lakers',
+        nickName: 'lakers',
+
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/220px-Los_Angeles_Lakers_logo.svg.png',
       },
       {
         teamName: 'Phoenix Suns',
+        nickName: 'suns',
+
         teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/5/56/Phoenix_Suns_2013.png',
       },
       {
         teamName: 'Sacramento Kings',
+        nickName: 'kings',
+
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/9/95/Kings_de_Sacramento_logo.svg/1200px-Kings_de_Sacramento_logo.svg.png',
       },
@@ -151,26 +180,31 @@ const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
     teams: [
       {
         teamName: 'Dallas Mavericks',
+        nickName: 'mavericks',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/b/b8/Mavericks_de_Dallas_logo.svg/150px-Mavericks_de_Dallas_logo.svg.png',
       },
       {
         teamName: 'Houston Rockets',
+        nickName: 'rockets',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Houston_Rockets_logo_2003.png/330px-Houston_Rockets_logo_2003.png',
       },
       {
         teamName: 'Memphis Grizzlies',
+        nickName: 'grizzlies',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Memphis_Grizzlies.svg/1200px-Memphis_Grizzlies.svg.png',
       },
       {
         teamName: 'New Orleans Pelicans',
+        nickName: 'pelicans',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/thumb/2/21/New_Orleans_Pelicans.png/200px-New_Orleans_Pelicans.png',
       },
       {
         teamName: 'San Antonio Spurs',
+        nickName: 'spurs',
         teamLogo:
           'https://upload.wikimedia.org/wikipedia/fr/0/0e/San_Antonio_Spurs_2018.png',
       },
