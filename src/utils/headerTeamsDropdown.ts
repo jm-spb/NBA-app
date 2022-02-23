ca9dc76e8de5f8103d85ffc7a245ae44935372e4
ivan <tuchel.ivan@gmail.com>
2022-02-23 12:54:06 +0300

    create teams list groped by division

---
+++ src/utils/headerTeamsDropdown.ts
@@ -0,0 +1,181 @@
+import { IHeaderTeamsDropdown } from '../types/headerTeamsTypes';
+
+const headerTeamsDropdown: IHeaderTeamsDropdown[] = [
+  {
+    division: 'Atlantic',
+    teams: [
+      {
+        teamName: 'Boston Celtics',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png',
+      },
+      {
+        teamName: 'Brooklyn Nets',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Brooklyn_Nets_newlogo.svg/130px-Brooklyn_Nets_newlogo.svg.png',
+      },
+      {
+        teamName: 'New York Knicks',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/d/dc/NY_Knicks_Logo_2011.png',
+      },
+      {
+        teamName: 'Philadelphia 76ers',
+        teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/4/48/76ers_2016.png',
+      },
+      {
+        teamName: 'Toronto Raptors',
+        teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/8/89/Raptors2015.png',
+      },
+    ],
+  },
+  {
+    division: 'Central',
+    teams: [
+      {
+        teamName: 'Chicago Bulls',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d1/Bulls_de_Chicago_logo.svg/1200px-Bulls_de_Chicago_logo.svg.png',
+      },
+      {
+        teamName: 'Cleveland Cavaliers',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/0/06/Cavs_de_Cleveland_logo_2017.png/150px-Cavs_de_Cleveland_logo_2017.png',
+      },
+      {
+        teamName: 'Detroit Pistons',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Detroit_Pistons_primary_logo_2017.png/150px-Detroit_Pistons_primary_logo_2017.png',
+      },
+      {
+        teamName: 'Indiana Pacers',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/c/cf/Pacers_de_l%27Indiana_logo.svg/1180px-Pacers_de_l%27Indiana_logo.svg.png',
+      },
+      {
+        teamName: 'Milwaukee Bucks',
+        teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/3/34/Bucks2015.png',
+      },
+    ],
+  },
+  {
+    division: 'Southeast',
+    teams: [
+      {
+        teamName: 'Atlanta Hawks',
+        teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png',
+      },
+      {
+        teamName: 'Charlotte Hornets',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/f/f3/Hornets_de_Charlotte_logo.svg/1200px-Hornets_de_Charlotte_logo.svg.png',
+      },
+      {
+        teamName: 'Miami Heat',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/1/1c/Miami_Heat_-_Logo.svg/1200px-Miami_Heat_-_Logo.svg.png',
+      },
+      {
+        teamName: 'Orlando Magic',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/b/bd/Orlando_Magic_logo_2010.png',
+      },
+      {
+        teamName: 'Washington Wizards',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/archive/d/d6/20161212034849%21Wizards2015.png',
+      },
+    ],
+  },
+  {
+    division: 'Northwest',
+    teams: [
+      {
+        teamName: 'Denver Nuggets',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/3/35/Nuggets_de_Denver_2018.png/180px-Nuggets_de_Denver_2018.png',
+      },
+      {
+        teamName: 'Minnesota Timberwolves',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d9/Timberwolves_du_Minnesota_logo_2017.png/200px-Timberwolves_du_Minnesota_logo_2017.png',
+      },
+      {
+        teamName: 'Oklahoma City Thunder',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/4/4f/Thunder_d%27Oklahoma_City_logo.svg/1200px-Thunder_d%27Oklahoma_City_logo.svg.png',
+      },
+      {
+        teamName: 'Portland Trail Blazers',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/en/thumb/2/21/Portland_Trail_Blazers_logo.svg/1200px-Portland_Trail_Blazers_logo.svg.png',
+      },
+      {
+        teamName: 'Utah Jazz',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/3/3b/Jazz_de_l%27Utah_logo.png',
+      },
+    ],
+  },
+  {
+    division: 'Pacific',
+    teams: [
+      {
+        teamName: 'Golden State Warriors',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Warriors_de_Golden_State_logo.svg/1200px-Warriors_de_Golden_State_logo.svg.png',
+      },
+      {
+        teamName: 'Los Angeles Clippers',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/d/d6/Los_Angeles_Clippers_logo_2010.png',
+      },
+      {
+        teamName: 'Los Angeles Lakers',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/220px-Los_Angeles_Lakers_logo.svg.png',
+      },
+      {
+        teamName: 'Phoenix Suns',
+        teamLogo: 'https://upload.wikimedia.org/wikipedia/fr/5/56/Phoenix_Suns_2013.png',
+      },
+      {
+        teamName: 'Sacramento Kings',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/9/95/Kings_de_Sacramento_logo.svg/1200px-Kings_de_Sacramento_logo.svg.png',
+      },
+    ],
+  },
+  {
+    division: 'Southwest',
+    teams: [
+      {
+        teamName: 'Dallas Mavericks',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/b/b8/Mavericks_de_Dallas_logo.svg/150px-Mavericks_de_Dallas_logo.svg.png',
+      },
+      {
+        teamName: 'Houston Rockets',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Houston_Rockets_logo_2003.png/330px-Houston_Rockets_logo_2003.png',
+      },
+      {
+        teamName: 'Memphis Grizzlies',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Memphis_Grizzlies.svg/1200px-Memphis_Grizzlies.svg.png',
+      },
+      {
+        teamName: 'New Orleans Pelicans',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/thumb/2/21/New_Orleans_Pelicans.png/200px-New_Orleans_Pelicans.png',
+      },
+      {
+        teamName: 'San Antonio Spurs',
+        teamLogo:
+          'https://upload.wikimedia.org/wikipedia/fr/0/0e/San_Antonio_Spurs_2018.png',
+      },
+    ],
+  },
+];
+
+export default headerTeamsDropdown;
