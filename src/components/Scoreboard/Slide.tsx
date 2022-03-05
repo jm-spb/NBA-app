2c09e53c76cb1b0d9b58ba7f3e794b9068c5f584
ivan <tuchel.ivan@gmail.com>
2022-03-05 22:39:49 +0300

    Slide: fix props & types

--- src/components/Scoreboard/Slide.tsx
+++ src/components/Scoreboard/Slide.tsx
@@ -1,60 +1,52 @@
 import React from 'react';
 
 import { CaretRightOutlined } from '@ant-design/icons';
-import { IScoreboardGames } from '../../types/apiNbaTypes';
+import { IScoreboardGamesRender } from '../../types/apiNbaTypes';
 
 const Slide = ({
-  startTimeUTC,
+  gameId,
+  startTime,
   statusGame,
-  hTeam,
-  vTeam,
-  homeWinCaret,
-  visitWinCaret,
-  hTeamRecord,
-  vTeamRecord,
-}: IScoreboardGames): JSX.Element => (
+  teamsInfo: { homeTeamInfo, visitorTeamInfo },
+}: IScoreboardGamesRender): JSX.Element => (
   <div className="slide">
-    <div className="slide-gameInfo">
-      <span className="slide-gameStatus">{`${startTimeUTC}`}</span>
+    <div className={`slide-gameInfo ${gameId}`}>
+      <span className="slide-gameStatus">{`${startTime}`}</span>
       <span className="slide-broadcaster">LEAGUE PASS</span>
     </div>
-
     <div className="slide-teamInfo">
       <div className="slide-team">
         <img
-          src={vTeam.logo}
-          alt={vTeam.fullName}
+          src={visitorTeamInfo.logo}
+          alt={visitorTeamInfo.fullName}
           width={16}
           height="auto"
           loading="lazy"
           className="slide-logo"
         />
-        <div className="slide-teamName">{vTeam.shortName}</div>
-        <CaretRightOutlined className={`slide-winner ${visitWinCaret}`} />
-
+        <div className="slide-teamName">{visitorTeamInfo.shortName}</div>
+        <CaretRightOutlined className={`slide-winner ${visitorTeamInfo.winCaret}`} />
         {statusGame === 'Finished' ? (
-          <div className="slide-score">{vTeam.score.points}</div>
+          <div className="slide-score">{visitorTeamInfo.points}</div>
         ) : (
-          <div className="slide-record">{vTeamRecord}</div>
+          <div className="slide-record">{`${visitorTeamInfo.win} - ${visitorTeamInfo.loss}`}</div>
         )}
       </div>
-
       <div className="slide-team">
         <img
-          src={hTeam.logo}
-          alt={hTeam.fullName}
+          src={homeTeamInfo.logo}
+          alt={homeTeamInfo.fullName}
           width={16}
           height="auto"
           loading="lazy"
           className="slide-logo"
         />
-        <div className="slide-teamName">{hTeam.shortName}</div>
-        <CaretRightOutlined className={`slide-winner ${homeWinCaret}`} />
-
+        <div className="slide-teamName">{homeTeamInfo.shortName}</div>
+        <CaretRightOutlined className={`slide-winner ${homeTeamInfo.winCaret}`} />
         {statusGame === 'Finished' ? (
-          <div className="slide-score">{hTeam.score.points}</div>
+          <div className="slide-score">{homeTeamInfo.points}</div>
         ) : (
-          <div className="slide-record">{hTeamRecord}</div>
+          <div className="slide-record">{`${homeTeamInfo.win} - ${homeTeamInfo.loss}`}</div>
         )}
       </div>
     </div>
