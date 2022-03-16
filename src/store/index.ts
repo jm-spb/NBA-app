43cd619fccd91434cd33ff07d5574b42aa6b21e5
ivan <tuchel.ivan@gmail.com>
2022-03-16 21:40:44 +0300

    store: delete apiBasketballService

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { apiBasketball } from '../services/apiBasketballService';
 import { apiNba } from '../services/apiNbaService';
 
 const rootReducer = combineReducers({
   [apiNba.reducerPath]: apiNba.reducer,
-  [apiBasketball.reducerPath]: apiBasketball.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiNba.middleware, apiBasketball.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiNba.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
