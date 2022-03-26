4e07c923760c7ae878b6cfa157fdab4c02df0f40
ivan <tuchel.ivan@gmail.com>
2022-03-26 22:01:21 +0300

    content: add footer content data

---
+++ src/content/footerContent.ts
@@ -0,0 +1,59 @@
+export const footerTopListData = [
+  {
+    title: 'NBA ORGANIZATION',
+    links: [
+      { name: 'NBA Official', path: 'https://official.nba.com/' },
+      { name: 'NBA Careers', path: 'https://careers.nba.com/' },
+    ],
+  },
+  {
+    title: 'NBA INITIATIVES',
+    links: [
+      { name: 'NBA Cares', path: 'https://cares.nba.com/' },
+      { name: 'Jr. NBA', path: 'https://jr.nba.com/' },
+      { name: 'NBA Voices', path: 'https://voices.nba.com/' },
+      { name: 'NBA FIT', path: 'https://fit.nba.com/' },
+      { name: 'Hoops for Troops', path: 'https://hoopsfortroops.nba.com/' },
+    ],
+  },
+  {
+    title: 'ACROSS THE LEAGUE',
+    links: [
+      { name: 'NBA Communications', path: 'https://pr.nba.com/' },
+      { name: 'Lockervision', path: 'https://lockervision.nba.com/' },
+      { name: 'NBA Transactions', path: 'https://www.nba.com/stats/transactions' },
+    ],
+  },
+  {
+    title: 'SHOP',
+    links: [
+      { name: 'Global Stores', path: 'https://global.nba.com/store-link-handler' },
+      { name: 'NYC Store', path: 'https://store.nba.com/' },
+      { name: 'NBA Auctions', path: 'https://auctions.nba.com/' },
+      { name: 'NBA Photostore', path: 'https://photostore.nba.com/' },
+    ],
+  },
+  {
+    title: 'SUBSCRIPTIONS',
+    links: [
+      { name: 'NBA TV', path: 'https://www.nba.com/watch/streaming-subscriptions' },
+      { name: 'League Pass', path: 'https://www.nba.com/watch/streaming-subscriptions' },
+      { name: 'Customer Support', path: 'https://support.watch.nba.com/hc/en-us' },
+    ],
+  },
+];
+
+export const footerBottomLegalsData = [
+  { legalName: 'Privacy Policy', legalLink: 'https://www.nba.com/privacy-policy' },
+  { legalName: 'Terms of Use', legalLink: 'https://www.nba.com/termsofuse' },
+  {
+    legalName: 'Accessibility and Closed Captions',
+    legalLink: 'https://nba.com/accessibility',
+  },
+  {
+    legalName: 'Do Not Sell My Personal Information',
+    legalLink:
+      'https://privacyportal.onetrust.com/webform/4b9fe937-73ed-43ed-863a-241ac6bfb482/784f7dd4-5b8b-4569-8087-4560abeab57e',
+  },
+  { legalName: 'Customer Support', legalLink: 'https://support.watch.nba.com/hc/en-us' },
+];
