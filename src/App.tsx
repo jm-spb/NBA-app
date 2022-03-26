ea3bbbc73a0778551df5b144089504241be527be
ivan <tuchel.ivan@gmail.com>
2022-03-26 22:32:21 +0300

    App: refactor basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,40 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-import './App.scss';
+import { Layout } from 'antd';
 import 'swiper/swiper.scss';
 
-import { Layout } from 'antd';
-
-import HeaderBar from './components/Header';
+import './App.scss';
+import HeaderBar from './components/HeaderBar';
 import Scoreboard from './components/Scoreboard';
 import HomePage from './pages/HomePage';
 import StatsPage from './pages/StatsPage';
 import StandingsPage from './pages/StandingsPage';
-import ErrorMsg from './components/ErrorMsg';
-
-import { nbaApi } from './services/NbaService';
+import FooterCustom from './components/FooterCustom';
 
 const App = (): JSX.Element => {
-  const { Header, Content, Footer } = Layout;
-  const { isError } = nbaApi.useFetchTeamsQuery('');
+  const { Content } = Layout;
+
   console.log('render');
-  // const isError = false;
-  // const isLoading = false;
 
   return (
     <Layout className="layout">
-      {isError ? <ErrorMsg /> : null}
-      <Header>
+      <div className="layout-top">
         <HeaderBar />
-      </Header>
-
-      <Scoreboard />
-      <div className="layout-adv" />
-
-      <Content>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/stats" element={<StatsPage />} />
-          <Route path="/standings" element={<StandingsPage />} />
-        </Routes>
-      </Content>
-
-      <Footer className="footer">NBA App ©2021 Created by JM-SPB</Footer>
+        <Scoreboard />
+      </div>
+      <div className="layout-content">
+        <div className="layout-adv" />
+        <Content className="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/stats" element={<StatsPage />} />
+            <Route path="/standings" element={<StandingsPage />} />
+          </Routes>
+        </Content>
+        <FooterCustom />
+      </div>
     </Layout>
   );
-
-  // return isError ? (
-  //   <h1>Ooops! Fail to fetch data!</h1>
-  // ) : isLoading ? (
-  //   <h1>Loading...</h1>
-  // ) : (
-  //   <Layout className="layout">
-  //     <Header>
-  //       <div className="logo">
-  //         <img src={Logo} width={100} height={35} alt="NBA Logo" />
-  //       </div>
-  //       <HeaderMenu />
-  //     </Header>
-  //     <Content>
-  //       <div className="site-layout-content">Content</div>
-  //     </Content>
-  //     <Footer className="footer">NBA App ©2021 Created by JM-SPB</Footer>
-  //   </Layout>
-  // );
 };
 
 export default App;
