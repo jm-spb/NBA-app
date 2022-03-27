8cef4d7babe2e91292ca4f4f459c39d041317180
ivan <tuchel.ivan@gmail.com>
2022-03-27 08:28:42 +0300

    HeaderTeams: create unique key & refactor

--- src/components/HeaderBar/HeaderTeams/index.tsx
+++ src/components/HeaderBar/HeaderTeams/index.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Menu, Dropdown } from 'antd';
+import { v4 as uuidv4 } from 'uuid';
 
-import headerTeamsDropdown from '../../../utils/headerTeamsDropdown';
+import headerTeamsDropdown from '../../../content/headerContent';
 import Division from './Division';
 
-const HeaderTeams = (): JSX.Element => {
-  const menuTeams = (
-    <Menu className="ant-dropdown-menu-teams">
-      {headerTeamsDropdown.map(({ division, teams }) => (
-        <Division key={division} division={division} teams={teams} />
-      ))}
-    </Menu>
-  );
+const menuTeams = (
+  <Menu className="ant-dropdown-menu-teams">
+    {headerTeamsDropdown.map(({ division, teams }) => (
+      <Division key={uuidv4()} division={division} teams={teams} />
+    ))}
+  </Menu>
+);
 
-  return (
-    <Dropdown
-      className="header-dropdown-link"
-      overlay={menuTeams}
-      getPopupContainer={(triggerNode) => triggerNode.parentNode as HTMLElement}
-      placement="bottomRight"
-    >
-      <span>Teams</span>
-    </Dropdown>
-  );
-};
+const HeaderTeams = (): JSX.Element => (
+  <Dropdown
+    className="header-dropdown-link"
+    overlay={menuTeams}
+    // getPopupContainer={(triggerNode) => triggerNode.parentNode as HTMLElement}
+    placement="bottomRight"
+  >
+    <span>Teams</span>
+  </Dropdown>
+);
 
 export default HeaderTeams;
