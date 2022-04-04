2254c5635afda86f74cc80635c925960e90506b9
ivan <tuchel.ivan@gmail.com>
2022-04-04 17:23:57 +0300

    create static content for LatestNews

---
+++ src/content/latestNewsContent.ts
@@ -0,0 +1,13 @@
+import { ISources } from '../types/contentTypes';
+
+const sources: ISources[] = [
+  { name: 'ESPN', query: 'espn' },
+  { name: 'NBA', query: 'nba' },
+  { name: 'SportsIllustrated', query: 'si' },
+  { name: 'SBNation', query: 'sbnation' },
+  { name: 'The Ringer', query: 'theringer' },
+  { name: 'RealGM', query: 'realgm' },
+  { name: 'HoopsHype', query: 'hoopshype' },
+];
+
+export default sources;
