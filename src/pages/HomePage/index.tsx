a3ef0892b99ad62791a4a83725d0b738c1f45767
ivan <tuchel.ivan@gmail.com>
2022-04-07 16:59:09 +0300

    HomePage: add LatesNews widget & styles

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import styles from './HomePage.module.scss';
 import MainCarousel from '../../components/MainCarousel';
+import LatestNews from '../../components/widgets/LatestNews';
 
-const HomePage = (): JSX.Element => <MainCarousel />;
+const HomePage = (): JSX.Element => (
+  <div>
+    <MainCarousel />
+    <section className={styles.section}>
+      <LatestNews />
+    </section>
+  </div>
+);
 
 export default HomePage;
