428fca034a2fe17605584bd8be890ca20fb65210
ivan <tuchel.ivan@gmail.com>
2022-04-12 20:13:57 +0300

    HomePage: add LatestNews & Standings widgets

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styles from './HomePage.module.scss';
 import MainCarousel from '../../components/MainCarousel';
-import LatestNews from '../../components/widgets/LatestNews';
+import LatestNews from '../../components/widgets/LatestNewsWidget';
+import StandingsWidget from '../../components/widgets/StandingsWidget';
 
 const HomePage = (): JSX.Element => (
   <div>
     <MainCarousel />
-    <section className={styles.section}>
-      <LatestNews />
-    </section>
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <LatestNews />
+      </section>
+      <div className={styles.widgets}>
+        <StandingsWidget />
+      </div>
+    </div>
   </div>
 );
 
