1261ec6639064f2035131a2abbedb859f13b786c
ivan <tuchel.ivan@gmail.com>
2022-04-16 18:49:54 +0300

    FooterCustom: create footerTopListResponsive element

--- src/components/FooterCustom/index.tsx
+++ src/components/FooterCustom/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Layout, List } from 'antd';
+import { Layout, List, Menu } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
 
 import styles from './FooterCustom.module.scss';
 import { footerBottomLegalsData, footerTopListData } from '../../content/footerContent';
+
+const { SubMenu } = Menu;
 
 const FooterContent = (): JSX.Element => {
   const { Footer } = Layout;
@@ -30,6 +32,28 @@ const FooterContent = (): JSX.Element => {
       )}
     />
   );
+
+  const footerTopListResponsive = (
+    <Menu mode="inline" theme="dark" className={styles.topResponsive}>
+      {footerTopListData.map(({ title, links }) => (
+        <SubMenu key={title} title={title}>
+          {links.map(({ name, path }) => (
+            <Menu.Item key={name}>
+              <a
+                className={styles.link}
+                href={path}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {name}
+              </a>
+            </Menu.Item>
+          ))}
+        </SubMenu>
+      ))}
+    </Menu>
+  );
+
   const footerBottomLegals = footerBottomLegalsData.map(({ legalName, legalLink }) => (
     <li key={uuidv4()}>
       <a
@@ -46,7 +70,9 @@ const FooterContent = (): JSX.Element => {
   return (
     <Footer className={styles.footer}>
       <div className={styles.container}>
+        {footerTopListResponsive}
         {footerTopList}
+
         <hr className={styles.divider} />
         <div className={styles.bottom}>
           <h5 className={styles.bottomHeading}>
@@ -55,17 +81,17 @@ const FooterContent = (): JSX.Element => {
           <ul className={styles.bottomLegals}>{footerBottomLegals}</ul>
           <p className={styles.bottomParagraph}>
             If you are having difficulty accessing any content on this website, please
-            visit our
+            visit our{' '}
+            <a
+              className={styles.bottomLink}
+              href="https://www.nba.com/accessibility"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Accessibility page.
+            </a>
           </p>
-          <a
-            className={styles.bottomLink}
-            href="https://www.nba.com/accessibility"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            Accessibility page.
-          </a>
+
           <p className={styles.bottomParagraph}>
             NBA.com is part of Warner Media, LLC&apos;s Turner Sports &amp; Entertainment
             Digital Network
