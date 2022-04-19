bdd9f18df1a90bd67d65817dc192a84173ebcb01
ivan <tuchel.ivan@gmail.com>
2022-04-19 22:03:29 +0300

    DatePickerCustom: refactor with antd DatePicker component

--- src/components/Scoreboard/DatePicker/index.tsx
+++ src/components/Scoreboard/DatePicker/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import dateFnsGenerateConfig from 'rc-picker/lib/generate/dateFns';
-import generatePicker from 'antd/es/date-picker/generatePicker';
-import 'antd/es/date-picker/style/index';
+import { DatePicker } from 'antd';
 
 import styles from './DatePicker.module.scss';
 import { IDatePickerProps } from '../../../types/scoreboardTypes';
 
-const GenerateDatePicker = generatePicker<Date>(dateFnsGenerateConfig);
-
-const DatePicker = ({ onDateChange }: IDatePickerProps): JSX.Element => {
-  const onChange = (date: Date | null, dateString: string) => {
+const DatePickerCustom = ({ onDateChange }: IDatePickerProps): JSX.Element => {
+  const onChange = (date: any | null, dateString: string) => {
     if (date) onDateChange(dateString);
   };
   return (
     <div className={styles.datePicker}>
       <span className={styles.date}>Game Date</span>
-      <GenerateDatePicker onChange={onChange} />
+      <DatePicker onChange={onChange} />
     </div>
   );
 };
 
-export default React.memo(DatePicker);
+export default React.memo(DatePickerCustom);
