e183544f92aa5fab1ca3b3e0ef59949620d04034
ivan <tuchel.ivan@gmail.com>
2022-04-22 12:59:57 +0300

    App: add routes with dynamic imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,45 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import 'swiper/swiper.scss';
 
-import './App.scss';
+import './styles/App.scss';
 import HeaderBar from './components/HeaderBar';
 import Scoreboard from './components/Scoreboard';
-import HomePage from './pages/HomePage';
-import StatsPage from './pages/StatsPage';
-import StandingsPage from './pages/StandingsPage';
 import FooterCustom from './components/FooterCustom';
+import Spinner from './components/Spinner';
+
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const StatsPage = React.lazy(() => import('./pages/StatsPage'));
+const StandingsPage = React.lazy(() => import('./pages/StandingsPage'));
+const TeamsPage = React.lazy(() => import('./pages/TeamsPage'));
+
+const { Content } = Layout;
 
 const App = (): JSX.Element => {
-  const { Content } = Layout;
-
   console.log('render');
 
   return (
     <Layout className="layout">
       <div className="layout-top">
         <HeaderBar />
-        <Scoreboard />
+        <section className="scoreboard">
+          <Scoreboard />
+        </section>
       </div>
+
       <div className="layout-content">
         <div className="layout-adv" />
         <Content className="main">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/stats" element={<StatsPage />} />
-            <Route path="/standings" element={<StandingsPage />} />
-          </Routes>
+          <React.Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/stats" element={<StatsPage />} />
+              <Route path="/standings" element={<StandingsPage />} />
+              <Route path="/teams" element={<TeamsPage />} />
+            </Routes>
+          </React.Suspense>
         </Content>
-        <FooterCustom />
       </div>
+      <FooterCustom />
     </Layout>
   );
 };
