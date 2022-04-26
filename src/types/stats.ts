487ce2a8e60fb149a42610ac72efb15ec163d8bc
ivan <tuchel.ivan@gmail.com>
2022-04-26 20:19:32 +0300

    add types to stats.ts

--- src/types/stats.ts
+++ src/types/stats.ts
@@ -1,9 +1,9 @@
 import { INbaPlayersNamesRender, INbaPlayersStatsRespone } from './apiNbaStats';
+import { ITeamsByDivisionContent, TeamBasicInfoType } from './contentTypes';
 
 export interface IStatsTableProps {
-  teamId: string;
-  selectedSeason: string;
-  seasonType: string;
+  dataSource: IStatsTableDataSource[];
+  isFetching: boolean;
 }
 
 export interface IStatsTableDataSource extends INbaPlayersStatsRespone {
@@ -12,4 +12,8 @@ export interface IStatsTableDataSource extends INbaPlayersStatsRespone {
 
 export interface ICreateTableDataSource<T> {
   (playersStatsData: T[], playersNamesData: INbaPlayersNamesRender[]): T[];
+}
+
+export interface IGetTeamPickerContent {
+  (content: ITeamsByDivisionContent[]): TeamBasicInfoType[];
 }
