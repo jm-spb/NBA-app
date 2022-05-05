fe7bee50dc2d32c1004ad4c9fe2b73c599f59abb
ivan <tuchel.ivan@gmail.com>
2022-05-05 23:26:30 +0300

    GameDetailsPage: create stats tables & add useAppSelector hook

--- src/pages/GameDetailsPage/index.tsx
+++ src/pages/GameDetailsPage/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Table } from 'antd';
+import { v4 as uuidv4 } from 'uuid';
 
 import styles from './GameDetailsPage.module.scss';
 import { useAppSelector } from '../../hooks/redux';
 import { IGameDetailsTeamStatsRender, IGameSummary } from '../../types/apiNbaTypes';
 import { apiNba } from '../../services/apiNbaService';
-
+import {
+  teamsAdditionalStatsTableColumns,
+  teamsBaseStatsTableColumns,
+} from '../../content/gameDetails';
 import ErrorMsg from '../../components/ErrorMsg';
 import Spinner from '../../components/Spinner';
 import GameSummary from './GameSummary';
-import TeamsBaseStats from './TeamsBaseStats';
 
 const GameDetailsPage = (): JSX.Element => {
   const { gameId } = useParams();
@@ -20,11 +24,11 @@ const GameDetailsPage = (): JSX.Element => {
     data: fetchedGameDetailsData,
     isError: gameDetailsIsError,
     isLoading: gameDetailsIsLoading,
+    isFetching: gameDetailsIsFetching,
   } = apiNba.useFetchNbaGameDetailsQuery(gameId as string);
 
   if (gameDetailsIsError)
     return <ErrorMsg failedData="teams statistics" notAvaliableService="apiNBA" />;
-
   if (isLoading || gameDetailsIsLoading) return <Spinner />;
 
   const gameById = gameSummaryData.find(
@@ -33,17 +37,52 @@ const GameDetailsPage = (): JSX.Element => {
 
   const { baseStats, additionalStats } =
     fetchedGameDetailsData as IGameDetailsTeamStatsRender;
-  // console.log(teamsStatsTyped);
-
-  // const  = teamsStatsTyped;
-  console.log(additionalStats);
+  const baseStatsDataSource = baseStats.map(({ name, ...rest }) => ({
+    name: <span className={styles.teamName}>{name}</span>,
+    ...rest,
+  }));
+  const additionalStatsDataSource = additionalStats.map(({ name, ...rest }) => ({
+    name: <span className={styles.teamName}>{name}</span>,
+    ...rest,
+  }));
 
   return (
     <div className={styles.container}>
       <GameSummary gameSummaryData={gameById} />
-      <div className={styles.stats}>
-        <TeamsBaseStats baseStats={baseStats} />
-      </div>
+      <section className={styles.statsWrapper}>
+        {gameDetailsIsFetching ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className={styles.stats}>
+              <h2 className={styles.title}>Base Teams Stats</h2>
+              <Table
+                className={styles.table}
+                rowClassName={styles.row}
+                rowKey={() => uuidv4()}
+                dataSource={baseStatsDataSource}
+                columns={teamsBaseStatsTableColumns}
+                pagination={false}
+                scroll={{ x: 1200 }}
+                size="small"
+              />
+            </div>
+            <div className={styles.stats}>
+              <h2 className={styles.title}>Additional Teams Stats</h2>
+              <Table
+                className={styles.table}
+                rowClassName={styles.row}
+                rowKey={() => uuidv4()}
+                dataSource={additionalStatsDataSource}
+                columns={teamsAdditionalStatsTableColumns}
+                pagination={false}
+                scroll={{ x: 580 }}
+                size="small"
+              />
+            </div>
+          </>
+        )}
+      </section>
     </div>
   );
 };
