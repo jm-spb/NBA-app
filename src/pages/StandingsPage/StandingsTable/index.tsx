b97c3376bfe461110a9df8cadfba8e66c97e2ac9
ivan <tuchel.ivan@gmail.com>
2022-05-06 16:51:18 +0300

    StandingsTables: refactor component

--- src/pages/StandingsPage/StandingsTable/index.tsx
+++ src/pages/StandingsPage/StandingsTable/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Table } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
+import classNames from 'classnames';
 
 import styles from './StandingsTable.module.scss';
 import {
@@ -10,18 +11,101 @@ import {
 } from '../../../content/standingsContent';
 import { filterTeamsByGroup } from '../../../utils/standings';
 import { IStandingsTableProps } from '../../../types/standingsTypes';
-import Spinner from '../../../components/Spinner';
 import { ITeamsStandingsRender } from '../../../types/apiNbaTypes';
+
+const createTableDataSource = (
+  team: ITeamsStandingsRender,
+  idx: number,
+  arr: ITeamsStandingsRender[],
+) => {
+  const positionByRank = arr.length > 6 ? team.conference.rank : team.division.rank;
+  const renderPosition = positionByRank > 0 ? positionByRank : idx + 1;
+
+  return {
+    key: team.teamId,
+    team: (
+      <div>
+        <span className={styles.teamRank}>{renderPosition}</span>
+        <img
+          className={styles.teamImage}
+          src={team.logo}
+          alt={team.fullName}
+          width={20}
+          height="auto"
+        />
+        <a
+          className={styles.teamFullName}
+          href={`https://nba.com/${team.nickName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {team.fullName}
+        </a>
+        <a
+          className={styles.teamShortName}
+          href={`https://nba.com/${team.nickName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {team.shortName}
+        </a>
+      </div>
+    ),
+    totalWin: team.totalWin,
+    totalLoss: team.totalLoss,
+    winPercentage: team.winPercentage,
+    gamesBehind: team.gamesBehind,
+    conf: `${team.conference.confWin}-${team.conference.confLoss}`,
+    div: `${team.division.divisionWin}-${team.division.divisionLoss}`,
+    home: `${team.homeWin}-${team.homeLoss}`,
+    road: `${team.awayWin}-${team.awayLoss}`,
+    last10: `${team.lastTenWin}-${10 - team.lastTenWin}`,
+    streak: `${team.winStreak ? 'W' : 'L'} ${team.streak}`,
+  };
+};
+
+const createTableTitle = (
+  groupTeams: ITeamsStandingsRender[],
+  idx: number,
+  arrayToMap: ITeamsStandingsRender[][],
+) => {
+  if (arrayToMap.length === 2)
+    return groupTeams[0].conference.name === 'east' ? (
+      <span className={styles.title}>Eastern Conference</span>
+    ) : (
+      <span className={styles.title}>Western Conference</span>
+    );
+  const divisionName = arrayToMap[idx][0].division.name;
+  const formatedDivisionName = `${divisionName[0].toUpperCase()}${divisionName.slice(1)}`;
+
+  return <span className={styles.title}>{formatedDivisionName} Division</span>;
+};
+
+const createStandingsTables = (
+  groupTeams: ITeamsStandingsRender[],
+  idx: number,
+  arrayToMap: ITeamsStandingsRender[][],
+) => {
+  const dataSource = groupTeams.map(createTableDataSource);
+
+  return (
+    <Table
+      key={uuidv4()}
+      className={styles.table}
+      rowClassName={classNames({ [styles.row]: arrayToMap.length === 2 })}
+      dataSource={dataSource}
+      columns={standingsTableColumns}
+      pagination={false}
+      scroll={{ x: 920 }}
+      title={() => createTableTitle(groupTeams, idx, arrayToMap)}
+    />
+  );
+};
 
 const StandingsTable = ({
   teamsStandings,
-  isFetching,
   groupBy,
 }: IStandingsTableProps): JSX.Element => {
-  if (isFetching) {
-    return <Spinner />;
-  }
-
   const selectedGroup = groupBy === 'conference' ? groupConference : groupDivision;
 
   const filteredTeamsByGroup = filterTeamsByGroup(
@@ -30,74 +114,7 @@ const StandingsTable = ({
     groupBy,
   ) as ITeamsStandingsRender[][];
 
-  const renderTeams = filteredTeamsByGroup.map((groupTeams, idx) => {
-    const dataSource = groupTeams.map((team) => ({
-      key: team.teamId,
-      team: (
-        <div>
-          <span className={styles.teamRank}>{team[groupBy].rank}</span>
-          <img
-            className={styles.teamImage}
-            src={team.logo}
-            alt={team.fullName}
-            width={20}
-            height="auto"
-          />
-          <a
-            className={styles.teamFullName}
-            href={`https://nba.com/${team.nickName}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {team.fullName}
-          </a>
-          <a
-            className={styles.teamShortName}
-            href={`https://nba.com/${team.nickName}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {team.shortName}
-          </a>
-        </div>
-      ),
-      totalWin: team.totalWin,
-      totalLoss: team.totalLoss,
-      winPercentage: team.winPercentage,
-      gamesBehind: team.gamesBehind,
-      conf: `${team.conference.confWin}-${team.conference.confLoss}`,
-      div: `${team.division.divisionWin}-${team.division.divisionLoss}`,
-      home: `${team.homeWin}-${team.homeLoss}`,
-      road: `${team.awayWin}-${team.awayLoss}`,
-      last10: `${team.lastTenWin}-${10 - team.lastTenWin}`,
-      streak: `${team.winStreak ? 'W' : 'L'} ${team.streak}`,
-    }));
-
-    return (
-      <Table
-        key={uuidv4()}
-        className={styles.table}
-        dataSource={dataSource}
-        columns={standingsTableColumns}
-        pagination={false}
-        scroll={{ x: 920 }}
-        title={() => {
-          if (groupBy === 'conference')
-            return selectedGroup[idx] === 'east'
-              ? 'Eastern Conference'
-              : 'Western Conference';
-
-          const divisionName = `${selectedGroup[idx][0].toUpperCase()}${selectedGroup[
-            idx
-          ].slice(1)}`;
-
-          return `${divisionName} Division`;
-        }}
-      />
-    );
-  });
-
-  return <div>{renderTeams}</div>;
+  return <>{filteredTeamsByGroup.map(createStandingsTables)}</>;
 };
 
 export default StandingsTable;
