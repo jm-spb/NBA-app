3c412f2c16835e55ef5efcd3eccf40037a03c4f4
ivan <tuchel.ivan@gmail.com>
2022-05-10 19:49:07 +0300

    ErrorMsg: add details prop & refactor component

--- src/components/ErrorMsg/index.tsx
+++ src/components/ErrorMsg/index.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import { Alert } from 'antd';
+import { Alert, Button } from 'antd';
+import classNames from 'classnames';
 import styles from './ErrorMsg.module.scss';
 import { IErrorMsgProps } from '../../types/errorMsg';
 
-const ErrorMsg = ({ failedData, notAvaliableService }: IErrorMsgProps): JSX.Element => (
-  <Alert
-    className={styles.error}
-    type="error"
-    message={`Unable to display ${failedData}, ${notAvaliableService} Service is not avaliable. Please try again later`}
-    banner
-  />
-);
+const ErrorMsg = ({
+  failedData,
+  notAvaliableService,
+  details,
+}: IErrorMsgProps): JSX.Element => {
+  const [isHidden, setIsHidden] = React.useState(true);
+  const handleOnClick = () => {
+    setIsHidden((state) => !state);
+  };
 
-export default ErrorMsg;
+  return (
+    <Alert
+      className={styles.error}
+      type="error"
+      message={
+        <strong
+          className={styles.message}
+        >{`Unable to display ${failedData}. Please try again later, or contact the developer for further assistance`}</strong>
+      }
+      description={
+        <div className={classNames({ [styles.isHidden]: isHidden })}>
+          <strong>Service: </strong> {notAvaliableService} <strong>Message: </strong>
+          {details}
+        </div>
+      }
+      showIcon
+      action={
+        <Button className={styles.details} size="small" danger onClick={handleOnClick}>
+          Details
+        </Button>
+      }
+    />
+  );
+};
+
+export default React.memo(ErrorMsg);
