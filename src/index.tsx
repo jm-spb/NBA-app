41aa152d1d0909f382eea717be1a21fce0917a7e
ivan <tuchel.ivan@gmail.com>
2022-05-13 23:30:21 +0300

    add PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './store';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
+serviceWorkerRegistration.register();
