42fe6cd05305a04fbb9ef622412bb28055c32aab
ivan <tuchel.ivan@gmail.com>
2022-05-18 20:43:20 +0300

    add NotFoundPage

---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Alert } from 'antd';
+import styles from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => (
+  <Alert
+    className={styles.alert}
+    message={<h1 className={styles.message}>Content Unavailable</h1>}
+    description={
+      <p className={styles.description}>
+        The page you are looking for is not available. Go to the
+        <Link to="/">homepage</Link>.
+      </p>
+    }
+    type="warning"
+    showIcon
+  />
+);
+
+export default NotFoundPage;
