b32f433004d0fb05788ee84593465561ade85729
ivan <tuchel.ivan@gmail.com>
2022-05-19 21:24:09 +0300

    add proper types

--- src/utils/formatDates.ts
+++ src/utils/formatDates.ts
@@ -1,14 +1,13 @@
 import { format, addDays, addYears } from 'date-fns';
-import { GameDateType } from '../types/scoreboardTypes';
+import { IGameDate } from '../types/scoreboardTypes';
 
 export const formatNextDay = (currentDay: string): string =>
   format(addDays(new Date(currentDay), 1), 'yyyy-MM-dd');
 
-export const currentDay = format(new Date(), 'yyyy-MM-dd');
+export const initialCurrentDay = format(new Date(), 'yyyy-MM-dd');
+export const initialNextDay = formatNextDay(initialCurrentDay);
 
-export const nextDay = formatNextDay(currentDay);
-
-export const formatDatesInGameDateSlide = (gamesDates: string[]): GameDateType[] =>
+export const formatDatesInGameDateSlide = (gamesDates: string[]): IGameDate[] =>
   gamesDates.map((gameDate) => {
     const [weekDay, monthDay] = format(new Date(gameDate), 'E-MMM dd').split('-');
     return { weekDay, monthDay };
