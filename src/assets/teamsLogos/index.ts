702827026a8b76096a149e5389e0da3cf0097910
ivan <tuchel.ivan@gmail.com>
2022-05-23 22:06:29 +0300

    add teams icons

---
+++ src/assets/teamsLogos/index.ts
@@ -0,0 +1,70 @@
+import celtics from './icons/celtics.svg';
+import knicks from './icons/knicks.svg';
+import nets from './icons/nets.svg';
+import raptors from './icons/raptors.svg';
+import sixers from './icons/sixers.svg';
+
+import bulls from './icons/bulls.svg';
+import cavaliers from './icons/cavaliers.svg';
+import pistons from './icons/pistons.svg';
+import pacers from './icons/pacers.svg';
+import bucks from './icons/bucks.svg';
+
+import hawks from './icons/hawks.svg';
+import hornets from './icons/hornets.svg';
+import heat from './icons/heat.svg';
+import magic from './icons/magic.svg';
+import wizards from './icons/wizards.svg';
+
+import nuggets from './icons/nuggets.svg';
+import timberwolves from './icons/timberwolves.svg';
+import thunder from './icons/thunder.svg';
+import blazers from './icons/blazers.svg';
+import jazz from './icons/jazz.svg';
+
+import warriors from './icons/warriors.svg';
+import clippers from './icons/clippers.svg';
+import lakers from './icons/lakers.svg';
+import suns from './icons/suns.svg';
+import kings from './icons/kings.svg';
+
+import mavericks from './icons/mavericks.svg';
+import rockets from './icons/rockets.svg';
+import grizzlies from './icons/grizzlies.svg';
+import pelicans from './icons/pelicans.svg';
+import spurs from './icons/spurs.svg';
+
+const teamsLogos: any = {
+  celtics,
+  knicks,
+  nets,
+  raptors,
+  sixers,
+  bulls,
+  cavaliers,
+  pistons,
+  pacers,
+  bucks,
+  hawks,
+  hornets,
+  heat,
+  magic,
+  wizards,
+  nuggets,
+  timberwolves,
+  thunder,
+  blazers,
+  jazz,
+  warriors,
+  clippers,
+  lakers,
+  suns,
+  kings,
+  mavericks,
+  rockets,
+  grizzlies,
+  pelicans,
+  spurs,
+};
+
+export default teamsLogos;
