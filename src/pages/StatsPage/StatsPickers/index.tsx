d78b776fb9809a27b8124a7fd37cbd6cceb2ba11
ivan <tuchel.ivan@gmail.com>
2022-06-18 09:55:39 +0300

    fix lighthouse score

--- src/pages/StatsPage/StatsPickers/index.tsx
+++ src/pages/StatsPage/StatsPickers/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Select } from 'antd';
-
 import styles from './StatsPickers.module.scss';
 import teamsByDivisionContent from '../../../content/teamsByDivisionContent';
 import { seasonTypes } from '../../../content/statsContent';
 import { getAvaliableStatsSeasons, getTeamPickerContent } from '../../../utils/stats';
 import { formatSeasons } from '../../../utils/standings';
+import { StatsPickersProps } from '../../../types/props';
 
 const { Option } = Select;
 
@@ -13,7 +13,7 @@ const StatsPickers = ({
   handleTeamChange,
   handleSeasonChange,
   handleSeasonTypeChange,
-}: any): JSX.Element => {
+}: StatsPickersProps): JSX.Element => {
   const teamPickerContent = getTeamPickerContent(teamsByDivisionContent);
   const formatedSeasons = formatSeasons(getAvaliableStatsSeasons());
 
@@ -45,36 +45,42 @@ const StatsPickers = ({
 
   return (
     <div className={styles.picker}>
-      <div>
-        <h5 className={styles.heading}>TEAM</h5>
+      <label htmlFor="team" className={styles.heading}>
+        TEAM
         <Select
+          id="team"
           className={styles.selector}
           placeholder="Select Team"
           onChange={onTeamChange}
+          aria-expanded={false}
         >
           {teamPicker}
         </Select>
-      </div>
-      <div>
-        <h5 className={styles.heading}>SEASON</h5>
+      </label>
+      <label htmlFor="season" className={styles.heading}>
+        SEASON
         <Select
+          id="season"
           className={styles.selector}
           defaultValue={formatedSeasons[0]}
           onChange={onSeasonChange}
+          aria-expanded={false}
         >
           {seasonPicker}
         </Select>
-      </div>
-      <div>
-        <h5 className={styles.heading}>SEASON TYPE</h5>
+      </label>
+      <label htmlFor="type" className={styles.heading}>
+        SEASON TYPE
         <Select
+          id="type"
           className={styles.selector}
           defaultValue={seasonTypes[0].type}
           onChange={onSeasonTypeChange}
+          aria-expanded={false}
         >
           {seasonTypePicker}
         </Select>
-      </div>
+      </label>
     </div>
   );
 };
