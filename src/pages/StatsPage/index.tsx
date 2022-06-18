44069f2e4cf55ea82d3ff6efa157b10de1f3b4ef
ivan <tuchel.ivan@gmail.com>
2022-06-18 09:57:14 +0300

    refactor component

--- src/pages/StatsPage/index.tsx
+++ src/pages/StatsPage/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Alert, Table } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
-
 import styles from './StatsPage.module.scss';
 import { apiNbaStats } from '../../services/apiNbaStats';
 import { formatSeasons } from '../../utils/standings';
 import { getAvaliableStatsSeasons } from '../../utils/stats';
 import { seasonTypes, statsTableColumns } from '../../content/statsContent';
 import { IPlayersStatsTableDataSource } from '../../types/stats';
-
 import ErrorMsg from '../../components/ErrorMsg';
 import Spinner from '../../components/Spinner';
 import StatsPickers from './StatsPickers';
@@ -16,46 +14,40 @@ import StatsPickers from './StatsPickers';
 const formatedSeasons = formatSeasons(getAvaliableStatsSeasons());
 const currentSeason = formatedSeasons[0];
 
+const warningMsg = (
+  <Alert
+    className={styles.warningMsg}
+    message="Due to lack of ApiNBA Stats data for current season, all statistics are available only for previous seasons"
+    type="warning"
+    showIcon
+    closable
+  />
+);
+
+const infoMsg = (
+  <Alert
+    className={styles.infoMsg}
+    message="Players Stats"
+    description="Please select team and season to view players statistics"
+    type="info"
+    showIcon
+  />
+);
+
 const StatsPage = (): JSX.Element => {
   const [teamShortName, setTeamShortName] = React.useState('');
   const [selectedSeason, setSelectedSeason] = React.useState(currentSeason);
   const [seasonType, setSeasonType] = React.useState(seasonTypes[0].query);
 
-  const handleTeamChange = React.useCallback((key: string) => {
-    setTeamShortName(key);
+  const handleTeamChange = React.useCallback((value: string) => {
+    setTeamShortName(value);
   }, []);
   const handleSeasonChange = React.useCallback((value: string) => {
     setSelectedSeason(value);
   }, []);
-  const handleSeasonTypeChange = React.useCallback((key: string) => {
-    setSeasonType(key);
+  const handleSeasonTypeChange = React.useCallback((value: string) => {
+    setSeasonType(value);
   }, []);
-
-  const warningMsg = React.useMemo(
-    () => (
-      <Alert
-        className={styles.warningMsg}
-        message="Due to lack of ApiNBA Stats data for current season, all statistics are available only for previous seasons"
-        type="warning"
-        showIcon
-        closable
-      />
-    ),
-    [],
-  );
-
-  const infoMsg = React.useMemo(
-    () => (
-      <Alert
-        className={styles.infoMsg}
-        message="Players Stats"
-        description="Please select team and season to view players statistics"
-        type="info"
-        showIcon
-      />
-    ),
-    [],
-  );
 
   const { data, error, isLoading, isFetching } = apiNbaStats.useFetchPlayersStatsQuery({
     teamShortName,
@@ -63,15 +55,15 @@ const StatsPage = (): JSX.Element => {
     seasonType,
   });
 
-  if (error && 'message' in error)
+  if (isLoading) return <Spinner loadingData="Stats table" />;
+  if (error)
     return (
       <ErrorMsg
+        error={error}
         failedData="Teams Stats"
         notAvaliableService="Api NBA Stats"
-        details={error.message as string}
       />
     );
-  if (isLoading) return <Spinner />;
 
   const playersByTeam = data as IPlayersStatsTableDataSource[] | null;
   let tableDataSource;
@@ -104,7 +96,10 @@ const StatsPage = (): JSX.Element => {
             columns={statsTableColumns}
             pagination={false}
             scroll={{ x: 1400 }}
-            loading={isFetching}
+            loading={{
+              spinning: isFetching,
+              tip: <span>Loading may take some time. Please wait...</span>,
+            }}
           />
         ) : (
           infoMsg
