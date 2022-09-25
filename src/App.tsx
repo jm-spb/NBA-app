a03d38a84c6a62300387ca6ef71ece216f0342e6
ivan <tuchel.ivan@gmail.com>
2022-09-25 12:20:06 +0300

    multiple changes in App 1) add lazy loaded GameDetailsPage, GameBoxScorePage, NotFoundPage 2) switch App.scss to App.less 3) remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import 'swiper/swiper.scss';
-
-import './styles/App.scss';
+import './styles/App.less';
 import HeaderBar from './components/HeaderBar';
 import Scoreboard from './components/Scoreboard';
 import FooterCustom from './components/FooterCustom';
@@ -13,37 +12,38 @@ const HomePage = React.lazy(() => import('./pages/HomePage'));
 const StatsPage = React.lazy(() => import('./pages/StatsPage'));
 const StandingsPage = React.lazy(() => import('./pages/StandingsPage'));
 const TeamsPage = React.lazy(() => import('./pages/TeamsPage'));
+const GameDetailsPage = React.lazy(() => import('./pages/GameDetailsPage'));
+const GameBoxScorePage = React.lazy(() => import('./pages/GameBoxScorePage'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 const { Content } = Layout;
 
-const App = (): JSX.Element => {
-  console.log('render');
-
-  return (
-    <Layout className="layout">
-      <div className="layout-top">
-        <HeaderBar />
-        <section className="scoreboard">
-          <Scoreboard />
-        </section>
+const App = (): JSX.Element => (
+  <Layout className="layout">
+    <div className="layout-top">
+      <HeaderBar />
+      <div className="scoreboard">
+        <Scoreboard />
       </div>
-
-      <div className="layout-content">
-        <div className="layout-adv" />
-        <Content className="main">
-          <React.Suspense fallback={<Spinner />}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/stats" element={<StatsPage />} />
-              <Route path="/standings" element={<StandingsPage />} />
-              <Route path="/teams" element={<TeamsPage />} />
-            </Routes>
-          </React.Suspense>
-        </Content>
-      </div>
-      <FooterCustom />
-    </Layout>
-  );
-};
+    </div>
+    <div className="layout-content">
+      <div className="layout-adv" />
+      <Content className="main">
+        <React.Suspense fallback={<Spinner loadingData="Page" />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/stats" element={<StatsPage />} />
+            <Route path="/standings" element={<StandingsPage />} />
+            <Route path="/teams" element={<TeamsPage />} />
+            <Route path="/game_details_:gameId" element={<GameDetailsPage />} />
+            <Route path="/game_box_score_:gameId" element={<GameBoxScorePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </React.Suspense>
+      </Content>
+    </div>
+    <FooterCustom />
+  </Layout>
+);
 
 export default App;
